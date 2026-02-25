4292964b837e9102effe6a782a9844a0c2cc3849
Pratik Sonawane <sonawanepratik1432@gmail.com>
2026-02-25 11:16:35 +0530

    Added basic chat API using Django

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+@csrf_exempt
+def chat_api(request):
+    if request.method == "POST":
+        body = json.loads(request.body)
+        user_message = body.get("message", "")
+
+        return JsonResponse({
+            "user_message": user_message,
+            "reply": "Hello! Chat API is working."
+        })
